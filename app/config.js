37c245d4c29916c0bce98eb662ce582d3213808a
Andy Jones <hello@awj.digital>
2018-12-06 16:39:26 +0000

    updates to designs, journeys and functionality

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Manage your personal licece',
+  serviceName: 'Manage your personal licence',
 
   // Default port that prototype runs on
   port: '3000',
