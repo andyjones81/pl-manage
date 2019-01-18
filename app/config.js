5a469b92491f51fcee5d258bf21fbcbe77fa987a
Andy Jones <hello@awj.digital>
2019-01-18 11:33:05 +0000

    updated cookie policy

--- app/config.js
+++ app/config.js
@@ -26,7 +26,8 @@ module.exports = {
   useHttps: 'true',
 
   // Cookie warning - update link to service's cookie page.
-  cookieText: 'GOV.UK uses cookies to make the site simpler. <a href="#">Find out more about cookies</a>',
+  cookieText: 'Gambling Commission uses cookies to make the site simpler. <a href="https://www.gamblingcommission.gov.uk/Footer/Privacy-and-cookies.aspx" target="_Blank">Find out more about cookies</a>',
+
 
   // Enable or disable Browser Sync
   useBrowserSync: 'true',
